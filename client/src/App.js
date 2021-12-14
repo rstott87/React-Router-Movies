81ef1c439cf9d5ced7d09dcbb5db450f5bb40c54
rstott87 <rstott87@yahoo.com>
2021-12-13 19:15:42 -0500

    struggling

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import axios from 'axios';
-
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
 
 export default function App () {
@@ -14,6 +16,7 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+         setMovieList(response.data.map(movies=>movies));
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -25,12 +28,28 @@ export default function App () {
   const addToSavedList = id => {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
-
   return (
     <div>
+      <Movie/>
       <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
-    </div>
+      <Link to="/">Link To Movie List</Link>
+      
+      <Route path="/">
+        <MovieList movies={movieList}/> 
+      </Route>
+      
+      <Route path="/movies/:id"> 
+        <Movie id={null}/>
+      </Route>
+    </div>   
   );
 }
+
+/*<MovieList movies={movieList}/>*/
+/*<Route path="/" component={MovieList}/>*/
+
+
+/**      <Route path="/" render={() =>  
+        <MovieList movies={movieList}> 
+          <Link path="/movies/"></Link>
+        </MovieList> */
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router';
+import { useRouteMatch } from 'react-router-dom'
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
+  const {id} = useParams();
+  const {path, url} = useRouteMatch();
 
-  let id = 1;
+
+  //let id = 1;
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
 
   useEffect(() => {
@@ -13,6 +18,7 @@ export default function Movie(props) {
       .then(response => {
         // Study this response with a breakpoint or log statements
         // and set the response data as the 'movie' slice of state
+      
       })
       .catch(error => {
         console.error(error);
@@ -20,7 +26,7 @@ export default function Movie(props) {
     // This effect should run every time time
     // the `id` changes... How could we do this?
   }, []);
-
+useEffect();
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = evt => { }
 
